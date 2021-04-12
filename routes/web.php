2ab600480698e1d2d1beb859c85dc1eefadbf2bf
Lenika Guerrero <guerrerov.lenika@sonora.edu.mx>
2021-04-12 12:29:26 -0700

    Rutas

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,30 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Definir una nueva Ruta
+Route::get('cursos', function () {
+    return "Bienvenido a la pagina de cursos";
+});
+
+//Ruta para formulario
+Route::get('cursos/create', function () {
+    return "En esta pagina podras crear un curso";
+});
+
+//Rutas por URL / Variables
+//Este tipo de rutas tiene que ir preferentemente al final para que no conflictue con las demas rutas
+
+// Route::get('cursos/{curso}', function ($curso) {
+//     return "Bienvenido al curso $curso";
+// });
+
+//Rutas con mas de una variable
+//Para hacer opcional una variable se asigna el signo: "?" despues del mismo e inicializar la variable
+Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria = null) {
+    if ($categoria) {
+        return "Bienvenido al curso $curso, de la categoria $categoria";
+    } else {
+        return "En esta pagina podras crear un curso";
+    }
+});
