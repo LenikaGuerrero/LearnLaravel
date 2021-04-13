e95d5cd3567876adb8873b81b261f660dc4d1393
Lenika Guerrero <guerrerov.lenika@sonora.edu.mx>
2021-04-13 11:24:25 -0700

    crear y eliminar migraciones

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,13 +16,14 @@ class CreateUsersTable extends Migration
         //nombre de la tabla, funcion que crea las columna de las tablas
         Schema::create('users', function (Blueprint $table) {
             $table->id(); //Integer Unsigned Increment
-            $table->string('name');//varchar(para indicar otra longitud se pone una coma ',' y el valor)
+            $table->string('name'); //varchar(para indicar otra longitud se pone una coma ',' y el valor)
             // $table->text('nombre'); //Almacena mas de 255 caracteres
-            $table->string('email')->unique();//Debe ser unico
-            $table->timestamp('email_verified_at')->nullable();//Guarda fechas al verificar correos - nullable pasa la propiedad de vacio (NULL)
-            $table->string('password');//varchar
-            $table->rememberToken();//varchar de longitud 100, marca "mantener la sesion iniciada"
-            $table->timestamps();//Al guardar se almacena la fecha en "created_at" y al modificar, pone la fecha en updated_at
+            $table->string('email')->unique(); //Debe ser unico
+            $table->timestamp('email_verified_at')->nullable(); //Guarda fechas al verificar correos - nullable pasa la propiedad de vacio (NULL)
+            $table->string('password'); //varchar
+            $table->string('avatar');
+            $table->rememberToken(); //varchar de longitud 100, marca "mantener la sesion iniciada"
+            $table->timestamps(); //Al guardar se almacena la fecha en "created_at" y al modificar, pone la fecha en updated_at
         });
     }
 
